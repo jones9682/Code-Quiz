044eb8505c4f31707085d5965613e48686971153
Bobby Jones Jr <jones9682@yahoo.com>
2020-06-15 14:27:05 -0400

    added 5 questions,choices,and answers

---
+++ questions.js
@@ -0,0 +1,45 @@
+// array of the questions, choices, and answers
+var questions = [
+  {
+    title:
+      "Which stormtrooper wasn't able to complete his mission in 'Star Wars: The Force Awakens'?",
+    choices: ["A. FN-2705()", "B. FN-2187()", "C. FN-1205()", "D. FN-9682()"],
+    answer: "B. FN-2187()",
+  },
+  {
+    title: "What kind of vehicle did Rey live in",
+    choices: [
+      "A. AT-ST()",
+      "B. Star Destroyer()",
+      "C. AT-AT()",
+      "D. Mon Calimari()",
+    ],
+    answer: "C. AT-AT()",
+  },
+  {
+    title: "According to Master Yoda, how many Sith are always out there?",
+    choices: ["A. 4()", "B. 3()", "C. 1()", "D. 2()"],
+    answer: "D. 2()",
+  },
+  {
+    title:
+      "What happened to Anakin Skywalker during the battle with Count Dooku?",
+    choices: [
+      "A. He lost his leg()",
+      "B. He lost his right arm()",
+      "C. He lost his right leg()",
+      "D. he died()",
+    ],
+    answer: "B. He lost his right arm()",
+  },
+  {
+    title: "Who killed Jabba?",
+    choices: [
+      "A. Han Solo()",
+      "B. C-3PO()",
+      "C. Luke Skywalker()",
+      "D. Princess Leia()",
+    ],
+    answer: "D. Princess Leia()",
+  },
+];
